329367363c9f4595ed4c6d2f05bee5f7b5eef7ed
CHP99 <duddndsla7777@gmail.com>
2024-11-27 00:59:26 +0900

    타일맵 제작

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,19 +7,18 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float moveSpeed = 5f;
-
     float horizontalMovement;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
-        
+
     }
 
     public void Move(InputAction.CallbackContext context)
